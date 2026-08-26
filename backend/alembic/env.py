b4407cfbe86b714fbeac68b1d99545cd70077a1f
Sidi Ould <estaghvirou.b@gmail.com>
2026-08-26 13:49:23 +0200

    Fix production database migrations

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -21,6 +21,13 @@ sys.path.append(
 # access to the values within the .ini file in use.
 config = context.config
 
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option(
+        "sqlalchemy.url",
+        database_url.replace("%", "%%"),
+    )
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
